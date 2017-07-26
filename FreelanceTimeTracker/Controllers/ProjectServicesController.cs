1150e50fe6eec5d9f9a04b9a5b64fc30fce41e3d
Mads Tinggaard <mads@tinggaard.org>
2017-07-26 21:20:26 +0200

    Updated the ProjectServicesController to repository pattern.

--- FreelanceTimeTracker/Controllers/ProjectServicesController.cs
+++ FreelanceTimeTracker/Controllers/ProjectServicesController.cs
@@ -9,26 +9,36 @@ using System.Web.Mvc;
 using FreelanceTimeTracker.Models;
 using Microsoft.AspNet.Identity;
 using Newtonsoft.Json;
+using Moq;
 
 namespace FreelanceTimeTracker.Controllers
 {
     public class ProjectServicesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private IProjectServiceRepository _repository;
+        public Func<string> GetUserName;
+
+
+        public ProjectServicesController()
+        {
+            _repository = new ProjectServiceRepository(new ApplicationDbContext());
+            GetUserName = () => User.Identity.GetUserName();
+        }
+
+        public ProjectServicesController(IProjectServiceRepository repository)
+        {
+            _repository = repository;
+        }
 
         // GET: ProjectServices
         [Authorize]
         public ActionResult Index()
         {
-            var userName = User.Identity.GetUserName();
+            var userName = GetUserName();
+            List<ProjectService> projectServices = _repository.GetProjectServicesForUserName(userName);
+            List<ProjectServiceViewModel> projectServiceVMList = new List<ProjectServiceViewModel>(projectServices.Count);
 
-            var projectServices = db.Projects
-                                    .Where(p => p.Client.ClientOwner.Equals(userName))
-                                    .SelectMany(p => p.ProjectServices.Select(ps => ps)).ToList();
-            List<ProjectServiceViewModel> projectServiceVMList = new List<ProjectServiceViewModel>(projectServices.Count());
-
-
-            foreach(ProjectService ps in projectServices)
+            foreach (ProjectService ps in projectServices)
             {
                 ProjectServiceViewModel psvm = new ProjectServiceViewModel();
                 psvm.ProjectId = ps.ProjectId;
@@ -48,9 +58,9 @@ namespace FreelanceTimeTracker.Controllers
         [Authorize]
         public ActionResult Create()
         {
-            var userName = User.Identity.GetUserName();
-            ViewBag.ClientName = new SelectList(db.Clients.Where(c => c.ClientOwner.Equals(userName)), "ClientId", "ClientName");
-            ViewBag.ServiceID = new SelectList(db.Services.Where(s => s.ServiceOwner.Equals(userName)), "ServiceiD", "ServiceName");
+            var userName = GetUserName();
+            ViewBag.ClientName = new SelectList(_repository.GetClientsBelongingToUserName(userName), "ClientId", "ClientName");
+            ViewBag.ServiceID = new SelectList(_repository.GetServicesBelongingToUserName(userName), "ServiceiD", "ServiceName");
             return View();
         }
 
@@ -65,13 +75,11 @@ namespace FreelanceTimeTracker.Controllers
 
             if (ModelState.IsValid)
             {
-                db.ProjectServices.Add(projectService);
-                db.SaveChanges();
+                _repository.AddProjectService(projectService);   
                 return RedirectToAction("Index");
             }
 
-            //ViewBag.ProjectId = new SelectList(db.Projects, "ProjectID", "ProjectName", projectService.ProjectId);
-            //ViewBag.ServiceID = new SelectList(db.Services, "ServiceiD", "ServiceOwner", projectService.ServiceID);
+            
             return View(projectService);
         }
 
@@ -83,13 +91,14 @@ namespace FreelanceTimeTracker.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ProjectService projectService = GetProjectService(ProjectId, ServiceID);
+            ProjectService projectService = _repository.GetProjectServiceByIds(ProjectId, ServiceID);
             if (projectService == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectID", "ProjectName", projectService.ProjectId);
-            ViewBag.ServiceID = new SelectList(db.Services, "ServiceiD", "ServiceOwner", projectService.ServiceID);
+            ViewBag.ProjectId = new SelectList(_repository.GetAllProjects(), "ProjectID", "ProjectName", projectService.ProjectId);
+            ViewBag.ServiceID = new SelectList(_repository.GetAllServices(), "ServiceiD", "ServiceOwner", projectService.ServiceID);
+
             return View(projectService);
         }
 
@@ -103,12 +112,11 @@ namespace FreelanceTimeTracker.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Entry(projectService).State = EntityState.Modified;
-                db.SaveChanges();
+                _repository.UpdateProjectService(projectService);
                 return RedirectToAction("Index");
             }
-            ViewBag.ProjectId = new SelectList(db.Projects, "ProjectID", "ProjectName", projectService.ProjectId);
-            ViewBag.ServiceID = new SelectList(db.Services, "ServiceiD", "ServiceOwner", projectService.ServiceID);
+             ViewBag.ProjectId = new SelectList(_repository.GetAllProjects(), "ProjectID", "ProjectName", projectService.ProjectId);
+            ViewBag.ServiceID = new SelectList(_repository.GetAllServices(), "ServiceiD", "ServiceOwner", projectService.ServiceID);
             return View(projectService);
         }
 
@@ -120,7 +128,7 @@ namespace FreelanceTimeTracker.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ProjectService projectService = GetProjectService(ProjectId, ServiceID);
+            ProjectService projectService = _repository.GetProjectServiceByIds(ProjectId, ServiceID);
             if (projectService == null)
             {
                 return HttpNotFound();
@@ -132,11 +140,10 @@ namespace FreelanceTimeTracker.Controllers
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         [Authorize]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(int? ProjectId, int? ServiceID)
         {
-            ProjectService projectService = db.ProjectServices.Find(id);
-            db.ProjectServices.Remove(projectService);
-            db.SaveChanges();
+            ProjectService projectService = _repository.GetProjectServiceByIds(ProjectId, ServiceID);
+            _repository.DeleteProjectService(projectService);
             return RedirectToAction("Index");
         }
 
@@ -150,30 +157,107 @@ namespace FreelanceTimeTracker.Controllers
                 PreserveReferencesHandling = PreserveReferencesHandling.Objects,
                 ObjectCreationHandling = ObjectCreationHandling.Auto
             };
-
-            var projects = db.Projects.Where(p => p.Client.ClientID == selectedClientId).ToList();
-            var  jsonProjects = JsonConvert.SerializeObject(projects, Formatting.Indented, _jsonSettings);
+            var projects = _repository.GetAllProjects().Where(p => p.Client.ClientID == selectedClientId).ToList();
+            
+            var jsonProjects = JsonConvert.SerializeObject(projects, Formatting.Indented, _jsonSettings);
 
 
             return Json(jsonProjects, JsonRequestBehavior.AllowGet);
         }
 
-        protected override void Dispose(bool disposing)
+        public static ProjectServicesController CreateProjectServiceControllerAs(string userName, IProjectServiceRepository repository)
         {
-            if (disposing)
+            var mock = new Mock<ControllerContext>();
+            var controller = new ProjectServicesController(repository)
             {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+                GetUserName = () => userName
+            };
+
+
+            controller.ControllerContext = mock.Object;
+            return controller;
+        }
+    }
+
+    public interface IProjectServiceRepository
+    {
+        List<ProjectService> GetProjectServicesForUserName(string userName);
+        ProjectService GetProjectServiceByIds(int? projectId, int? serviceId);
+        void AddProjectService(ProjectService projectService);
+        void UpdateProjectService(ProjectService projectService);
+        void DeleteProjectService(ProjectService projectService);
+        List<Client> GetClientsBelongingToUserName(string userName);
+        List<Service> GetServicesBelongingToUserName(string userName);
+        List<Project> GetAllProjects();
+        List<Service> GetAllServices();
+
+    }
+
+    public class ProjectServiceRepository : IProjectServiceRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProjectServiceRepository(ApplicationDbContext context)
+        {
+            _context = context;
         }
 
-        private ProjectService GetProjectService(int? ProjectId, int? ServiceID)
+
+        public void AddProjectService(ProjectService projectService)
         {
-            if(ProjectId == null || ServiceID == null)
+            _context.ProjectServices.Add(projectService);
+            _context.SaveChanges();
+        }
+
+        public void DeleteProjectService(ProjectService projectService)
+        {
+            _context.ProjectServices.Remove(projectService);
+            _context.SaveChanges();
+        }
+
+        public List<Project> GetAllProjects()
+        {
+            return _context.Projects.ToList();
+        }
+
+        public List<Service> GetAllServices()
+        {
+            return _context.Services.ToList();
+        }
+
+        public List<Client> GetClientsBelongingToUserName(string userName)
+        {
+            var clients = from c in _context.Clients select c;
+
+            return clients.Where(c => c.ClientOwner.Equals(userName)).ToList();
+        }
+
+        public ProjectService GetProjectServiceByIds(int? projectId, int? serviceId)
+        {
+            if (projectId == null || serviceId == null)
             {
                 return null;
             }
-           return db.ProjectServices.Where(ps => ps.ProjectId == ProjectId && ps.ServiceID == ServiceID).ToList()[0];
+            return _context.ProjectServices.Where(ps => ps.ProjectId == projectId && ps.ServiceID == serviceId).ToList()[0];
+        }
+
+        public List<ProjectService> GetProjectServicesForUserName(string userName)
+        {
+            return _context.Projects.Where(p => p.Client.ClientOwner.Equals(userName))
+                                                .SelectMany(p => p.ProjectServices.Select(ps => ps)).ToList();
+        }
+
+        public List<Service> GetServicesBelongingToUserName(string userName)
+        {
+            var services = from s in _context.Services select s;
+
+            return services.Where(s => s.ServiceOwner.Equals(userName)).ToList();
+        }
+
+        public void UpdateProjectService(ProjectService projectService)
+        {
+            _context.Entry(projectService).State = EntityState.Modified;
+            _context.SaveChanges();
         }
     }
 }
